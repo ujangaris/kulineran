23c0476613933698e5e19d3f1fa3784a698934b3
ujangaris <ujangaja@gmail.com>
2021-02-26 21:39:22 +0700

    5 Membuat card Product | footer

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
   <div id="app">
    
     <router-view/>
+    <Footer/>
   </div>
 </template>
+<script>
+import Footer from "@/components/Footer.vue";
+export default {
+  components: {
+    Footer
+  }
+}
+</script>
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="mb-5 mt-5">
+      <hr>
+      <div class="container">
+          <div class="row">
+              <div class="col text-center">
+                  2021 Copyright Ujang Arisandi
+              </div>
+          </div>
+      </div>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style>
+
+</style>
