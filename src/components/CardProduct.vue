535cd38f45139f4a49b594fa8fed3d04c0c95891
ujangaris <ujangaja@gmail.com>
2021-02-26 21:26:11 +0700

    5 Membuat card Product | menampilkan data best produt, pada Card

--- src/components/CardProduct.vue
+++ src/components/CardProduct.vue
@@ -1,5 +1,12 @@
 <template>
-  <h2>{{product.nama}}</h2>
+ <div class="card shadow card-product" >
+  <img :src="'assets/images/'+product.gambar" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">{{ product.nama }}</h5>
+    <p class="card-text">Harga : Rp. {{product.harga}}</p>
+    <a href="#" class="btn btn-success"><b-icon-cart></b-icon-cart> Pesan</a>
+  </div>
+</div>
 </template>
 
 <script>
