9d79d13b9d72bbf63db23b9a5404df5ab667ab7e
ujangaris <ujangaja@gmail.com>
2021-03-01 17:45:10 +0700

    7 Membuat halaman product detail | Menampilkan detail makanan

--- src/views/FoodDetail.vue
+++ src/views/FoodDetail.vue
@@ -2,6 +2,7 @@
   <div class="food-detail">
     <Navbar />
     <div class="container">
+        <!-- breadcrumb -->
       <div class="row mt-5">
         <div class="col">
           <nav aria-label="breadcrumb">
@@ -19,17 +20,48 @@
           </nav>
         </div>
       </div>
+        <!-- breadcrumb -->
+
+    <div class="row">
+        <div class="col-md-6">
+            <img :src="'../assets/images/'+product.gambar" class="img-fluid shadow">
+        </div>
+        <div class="col-md-6">
+            <h2><strong>{{ product.nama }}</strong></h2>
+            <h4>Harga : <strong>{{product.harga}}</strong></h4>
+        </div>
+    </div>
     </div>
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import axios from 'axios';
 
 export default {
   name: "FoodDetail",
   components: {
     Navbar,
+  },
+  data(){
+      return{
+          product:{}
+      }
+  },
+  methods:{
+      setProducts(data){
+          this.product = data
+      }
+  },
+   mounted() {
+    axios
+      .get("http://localhost:3000/products/"+this.$route.params.id)
+      .then((response) =>
+        // handle success
+        this.setProducts(response.data)
+      )
+      .catch((error) => console.log(error));
   },
 };
 </script>
