282237b746443624be8087b265c59abcea1175f7
ujangaris <ujangaja@gmail.com>
2021-03-01 21:01:29 +0700

    9 Membuat halaman keranjang | Menampilkan seluruh pesanan pada halaman keranjang

--- src/views/Keranjang.vue
+++ src/views/Keranjang.vue
@@ -1,17 +1,109 @@
 <template>
-<div class="keranjang">
-    <Navbar/>
-    <h2>Keranjang</h2>
-</div>
+  <div class="keranjang">
+    <Navbar />
+    <div class="container">
+      <!-- breadcrumb -->
+      <div class="row mt-5">
+        <div class="col">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                <router-link to="/" class="text-dark">Home</router-link>
+              </li>
+              <li class="breadcrumb-item">
+                <router-link to="/foods" class="text-dark">Foods</router-link>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
+                Keranjang
+              </li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+      <!-- breadcrumb -->
+
+      <div class="row">
+        <div class="col">
+          <h2>Keranjang <strong>Saya </strong></h2>
+          <div class="table-responsive mt-3">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Foto</th>
+                  <th scope="col">Nama Makanan</th>
+                  <th scope="col">Keterangan</th>
+                  <th scope="col">Jumlah</th>
+                  <th scope="col">Harga</th>
+                  <th scope="col">Total Harga</th>
+                  <th scope="col">Hapus</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(keranjang, index) in keranjangs"
+                  :key="keranjang.id"
+                >
+                  <th>{{ index + 1 }}</th>
+                  <td>
+                    <img
+                      :src="'../assets/images/' + keranjang.products.gambar"
+                      class="img-fluid shadow"
+                      width="250px"
+                    />
+                  </td>
+                  <td>
+                    <strong>{{ keranjang.products.nama }}</strong>
+                  </td>
+                  <td>
+                    {{ keranjang.keterangan ? keranjang.keterangan : "-" }}
+                  </td>
+                  <td>
+                    {{ keranjang.jumlah_pemesanan}}
+                  </td>
+                  <td align="right">
+                    Rp. {{ keranjang.products.harga}}
+                  </td>
+                  <td align="right">
+                    <strong>Rp. {{ keranjang.products.harga * keranjang.jumlah_pemesanan}}</strong>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import axios from "axios";
 
 export default {
   name: "Keranjang",
   components: {
-    Navbar
+    Navbar,
+  },
+  data() {
+    return {
+      keranjangs: [],
+    };
+  },
+  methods: {
+    setKeranjangs(data) {
+      this.keranjangs = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/keranjangs")
+      .then((response) =>
+        // handle success
+        this.setKeranjangs(response.data)
+      )
+      .catch((error) => console.log(error));
   },
 };
 </script>
