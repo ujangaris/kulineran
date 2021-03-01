0e5575431c8b843ae474a3ab1bcc7c84843efc0b
ujangaris <ujangaja@gmail.com>
2021-03-01 15:10:16 +0700

    7 Membuat halaman product detail | membuat breadcrumb

--- src/views/FoodDetail.vue
+++ src/views/FoodDetail.vue
@@ -1,13 +1,38 @@
 <template>
-  <h1>Food Detail {{ $route.params.id}}</h1>
+  <div class="food-detail">
+    <Navbar />
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                  <router-link to="/" class="text-dark">Home</router-link>
+                </li>
+                <li class="breadcrumb-item">
+                  <router-link to="/foods" class="text-dark">Foods</router-link>
+                </li>
+                <li class="breadcrumb-item active" aria-current="page">
+                Food Order
+                </li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import Navbar from "@/components/Navbar.vue";
 
-}
+export default {
+  name: "FoodDetail",
+  components: {
+    Navbar,
+  },
+};
 </script>
 
 <style>
-
 </style>
