7f0434376e13b828f623e9d74cca544fd67c8e03
ujangaris <ujangaja@gmail.com>
2021-03-01 20:21:26 +0700

    8 Membuat pesanan makanan |menampilkan jumlah pesanan pada navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light ">
+  <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
       <a class="navbar-brand" href="#">Kulineran</a>
       <button
@@ -25,9 +25,9 @@
         <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <router-link class="nav-link" to="/keranjang">
-            Keranjang
-            <b-icon-bag></b-icon-bag>
-            <span class="badge badge-success ml-2">0</span>
+              Keranjang
+              <b-icon-bag></b-icon-bag>
+              <span class="badge badge-success ml-2">{{ jumlah_pesanans.length  }}</span>
             </router-link>
           </li>
         </ul>
@@ -37,8 +37,29 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Navbar",
+  data() {
+    return {
+      jumlah_pesanans: [],
+    };
+  },
+  methods: {
+    setJumlah(data) {
+      this.jumlah_pesanans= data;
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/keranjangs")
+      .then((response) =>
+        // handle success
+        this.setJumlah(response.data)
+      )
+      .catch((error) => console.log(error));
+  },
 };
 </script>
 
