a167f3f2dabdcbcfeec49e4baefac17a20e2ab85
ujangaris <ujangaja@gmail.com>
2021-03-01 17:54:34 +0700

    7 Membuat halaman product detail | Membuat jumlah pesanan dan keterangan

--- src/views/FoodDetail.vue
+++ src/views/FoodDetail.vue
@@ -2,61 +2,80 @@
   <div class="food-detail">
     <Navbar />
     <div class="container">
-        <!-- breadcrumb -->
+      <!-- breadcrumb -->
       <div class="row mt-5">
         <div class="col">
           <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item">
-                  <router-link to="/" class="text-dark">Home</router-link>
-                </li>
-                <li class="breadcrumb-item">
-                  <router-link to="/foods" class="text-dark">Foods</router-link>
-                </li>
-                <li class="breadcrumb-item active" aria-current="page">
+              <li class="breadcrumb-item">
+                <router-link to="/" class="text-dark">Home</router-link>
+              </li>
+              <li class="breadcrumb-item">
+                <router-link to="/foods" class="text-dark">Foods</router-link>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
                 Food Order
-                </li>
+              </li>
             </ol>
           </nav>
         </div>
       </div>
-        <!-- breadcrumb -->
+      <!-- breadcrumb -->
 
-    <div class="row">
+      <div class="row mt-2">
         <div class="col-md-6">
-            <img :src="'../assets/images/'+product.gambar" class="img-fluid shadow">
+          <img
+            :src="'../assets/images/' + product.gambar"
+            class="img-fluid shadow"
+          />
         </div>
         <div class="col-md-6">
-            <h2><strong>{{ product.nama }}</strong></h2>
-            <h4>Harga : <strong>{{product.harga}}</strong></h4>
+          <h2>
+            <strong>{{ product.nama }}</strong>
+          </h2>
+          <h4>
+            Harga : <strong>{{ product.harga }}</strong>
+          </h4>
+          <form action="">
+              <div class="form-group">
+                <label for="jumlah_pemesanan">Jumlah Pemesanan</label>
+                <input type="number" name="" id="" class="form-control" >
+              </div>
+              <div class="form-group">
+                <label for="keterangan">Keterangan</label>
+                <textarea name="" class="form-control" placeholder="ket: pedas, nasi  1/2.."></textarea>
+
+              </div>
+                <button type="submit" class="btn btn-success"><b-icon-cart></b-icon-cart> Pesan</button>
+          </form>
         </div>
-    </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   name: "FoodDetail",
   components: {
     Navbar,
   },
-  data(){
-      return{
-          product:{}
-      }
+  data() {
+    return {
+      product: {},
+    };
   },
-  methods:{
-      setProducts(data){
-          this.product = data
-      }
+  methods: {
+    setProducts(data) {
+      this.product = data;
+    },
   },
-   mounted() {
+  mounted() {
     axios
-      .get("http://localhost:3000/products/"+this.$route.params.id)
+      .get("http://localhost:3000/products/" + this.$route.params.id)
       .then((response) =>
         // handle success
         this.setProducts(response.data)
