0c056c1497ef7370f9d945d9cb62437e26f9b1d3
ujangaris <ujangaja@gmail.com>
2021-03-02 19:19:14 +0700

    Upload Imgae and db assets

--- src/views/PesananSukses.vue
+++ src/views/PesananSukses.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="pesanan-sukses">
     <Navbar />
-    <h2>Pesanan Sukses!</h2>
+    <div class="container">
+      <div class="row justify-content-center mt-5">
+        <div class="col text-center">
+          <img src="../assets/images/menunggu.png" width="300" />
+          <h2><strong>Sukses!!</strong></h2>
+          <h4>Pesanan Anda segera diproses <br> Selamat Menunggu</h4>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
